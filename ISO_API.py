7c40e9b89282f3cd2c7410bfecac3b5165267cd4
brayand <bray.andrew.0@gmail.com>
2018-01-07 13:16:28 -0600

    Fixed NE enviro var setting and password. Added CAISO capability.

--- ISO_API.py
+++ ISO_API.py
@@ -9,22 +9,30 @@ import pandas as pd
 import os
 
 #get logins
+#Seperate file which git ignores in .gitignore called secrets.txt
+#contains ISONE_USERNAME=username
+#         ISONE_PASSWORD=password
 with open('secrets.txt') as f:
     lines = f.readlines()
     for line in lines:
         (key,val) = line.split(":")
         os.environ[key] = str(val).rstrip("\n")
 
-#Get hook
+#Get NE hook
 isone = client_factory('ISONE', timeout_seconds=60)
-
-print(isone)
-
 data = isone.get_load(latest=True)
-
 df = pd.DataFrame(data)
 
-print(df)
+#print(df)
 
+#Get CAISO hook
+caiso = client_factory('CAISO')
+caiso_load = isone.get_load(latest=True)
+caiso_generation = isone.get_generation(latest=True)
+#caiso_trade = isone.get_trade(latest=True)
+caiso_load_df = pd.DataFrame(caiso_load)
+print(caiso_load_df)
+caiso_generation_df = pd.DataFrame(caiso_generation)
+#caiso_trade_df = pd.DataFrame(caiso_trade)
 
 
