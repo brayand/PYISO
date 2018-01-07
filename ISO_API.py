a989c8f4a9c940d0351d9fa6666d70b3a665a7db
brayand <bray.andrew.0@gmail.com>
2018-01-07 10:10:09 -0600

    First implementation for data retrieval of non login load data

--- ISO_API.py
+++ ISO_API.py
@@ -4,11 +4,27 @@ Spyder Editor
 
 This is a temporary script file.
 """
-import pyiso
+from pyiso import client_factory
+import pandas as pd
+import os
 
+#get logins
+with open('secrets.txt') as f:
+    lines = f.readlines()
+    for line in lines:
+        (key,val) = line.split(":")
+        os.environ[key] = str(val).rstrip("\n")
 
+#Get hook
+isone = client_factory('ISONE', timeout_seconds=60)
 
+print(isone)
 
+data = isone.get_load(latest=True)
+
+df = pd.DataFrame(data)
+
+print(df)
 
 
 
